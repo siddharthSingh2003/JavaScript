fa1a418b6a48debb28137e36d14573761864e776
Siddharth Singh <97836894+siddharthSingh2003@users.noreply.github.com>
2023-12-10 09:34:21 +0000

    variable

---
+++ javas/variable.js
@@ -0,0 +1,6 @@
+const accountID=1435
+let accountEmail="sid@gmail.com"
+var accountPassword="12345"
+accountCity="new"
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountCity])
