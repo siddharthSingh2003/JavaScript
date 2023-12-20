165949cff0a0c31dfec9933266973234435b9442
Siddharth Singh <97836894+siddharthSingh2003@users.noreply.github.com>
2023-12-20 16:32:11 +0000

    array

--- javas/arrays.js
+++ javas/arrays.js
@@ -1,18 +1,29 @@
 const myarr=[0,1,2,3,4,5]
 
-console.log(myarr[3])
-// methods
+// console.log(myarr[3])
+// // methods
 
-myarr.push(6)
-console.log(myarr)
-myarr.pop()
-console.log(myarr)
-myarr.unshift(10)
-console.log(myarr)
-myarr.shift()
-console.log(myarr)
+// myarr.push(6)
+// console.log(myarr)
+// myarr.pop()
+// console.log(myarr)
+// myarr.unshift(10)
+// console.log(myarr)
+// myarr.shift()
+// console.log(myarr)
 
-console.log(myarr.includes(5))
+// console.log(myarr.includes(5))
 
-console.log(myarr.indexOf(5))
+// console.log(myarr.indexOf(5))
+
+
+// slice, splice
+
+const newarr=myarr.slice(0,3)
+
+console.log(newarr)
+
+const newarr1=myarr.splice(2,3)
+
+console.log(newarr1)
 
