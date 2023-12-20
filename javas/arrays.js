964201e7dece5e398d6c400518312a1c82bfe7f9
Siddharth Singh <97836894+siddharthSingh2003@users.noreply.github.com>
2023-12-20 15:57:39 +0000

    array

---
+++ javas/arrays.js
@@ -0,0 +1,18 @@
+const myarr=[0,1,2,3,4,5]
+
+console.log(myarr[3])
+// methods
+
+myarr.push(6)
+console.log(myarr)
+myarr.pop()
+console.log(myarr)
+myarr.unshift(10)
+console.log(myarr)
+myarr.shift()
+console.log(myarr)
+
+console.log(myarr.includes(5))
+
+console.log(myarr.indexOf(5))
+
