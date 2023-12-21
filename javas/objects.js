43e5e5bbd162e36fc9421417848576986ebf7d18
Siddharth Singh <97836894+siddharthSingh2003@users.noreply.github.com>
2023-12-21 10:27:18 +0000

    ok

--- javas/objects.js
+++ javas/objects.js
@@ -1,13 +1,18 @@
 // objects literals
 
+const mySym = Symbol("newSym")
+
 const JsUser={
     key:"values",  //key is string
     name:"sid",
     age: 20,
     location: "gotham",
     isLoggedIn: false,
-    "full name": "Siddharth Singh"
+    "full name": "Siddharth Singh",
+    [mySym]:"my Sym"
 }
 
 console.log(JsUser.isLoggedIn)
 console.log(JsUser["full name"])
+console.log(JsUser);
+console.log(typeof mySym);
