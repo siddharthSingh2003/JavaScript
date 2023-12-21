01fa2b48e2f6a2b9a74e714f04b6ca8ebb6f898d
Siddharth Singh <97836894+siddharthSingh2003@users.noreply.github.com>
2023-12-21 10:04:15 +0000

    objests

---
+++ javas/objects.js
@@ -0,0 +1,13 @@
+// objects literals
+
+const JsUser={
+    key:"values",  //key is string
+    name:"sid",
+    age: 20,
+    location: "gotham",
+    isLoggedIn: false,
+    "full name": "Siddharth Singh"
+}
+
+console.log(JsUser.isLoggedIn)
+console.log(JsUser["full name"])
