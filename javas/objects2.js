abe7547c54815144ddd5fd1dc3bc6bd0129498df
Siddharth Singh <97836894+siddharthSingh2003@users.noreply.github.com>
2023-12-21 12:21:59 +0000

    nesting

---
+++ javas/objects2.js
@@ -0,0 +1,20 @@
+//singelton
+
+const tinderUser= new Object()
+
+tinderUser.id="01";
+tinderUser.fullName="shak"
+
+tinderUser.User={
+    FirstName:"shakis",
+    lastName:"ram",
+    Address:{
+        Home:"123 block C",
+        Area:{
+            city:"brooklyn",
+            neighbour:"New York"
+        }
+    }
+}
+//console.log(tinderUser.User.Address.Area.city)
+
