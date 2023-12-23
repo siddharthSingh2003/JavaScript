d8f0cc7335c5914afdf4a8b1133b893e36fcf514
Siddharth Singh <97836894+siddharthSingh2003@users.noreply.github.com>
2023-12-23 09:20:50 +0000

    obj

--- javas/objects2.js
+++ javas/objects2.js
@@ -18,3 +18,10 @@ tinderUser.User={
 }
 //console.log(tinderUser.User.Address.Area.city)
 
+const obj1={1:"a", 2:"b"}
+const obj2={3:"c", 4:"d"}
+Object.assign(obj1,obj2)
+console.log(obj1)
+
+// const obj3={...obj1,...obj2}
+// console.log(obj3)
