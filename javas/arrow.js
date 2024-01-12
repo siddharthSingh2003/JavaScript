a22d846d29640b730ef6c1e1c8fed6322151e95d
sidddharthsingh <sidsingh1679@gmail.com>
2024-01-12 19:45:37 +0530

    this

--- javas/arrow.js
+++ javas/arrow.js
@@ -1,9 +1,13 @@
 const user = { 
-    username: 'siddhart',
+    username: 'siddharth',
     price: 99,
 
     welcomeMessage: function () {
         console.log(`${this.username} , welocme to website`)
+        console.log(this)
     }
 }
 
+user.welcomeMessage()
+user.username = "sid"
+user.welcomeMessage()     
