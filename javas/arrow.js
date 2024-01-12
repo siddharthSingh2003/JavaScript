60a1cb9f92bf67f71f56bb9a71854d5901df1d32
sidddharthsingh <sidsingh1679@gmail.com>
2024-01-12 19:55:05 +0530

    arrow2

--- javas/arrow.js
+++ javas/arrow.js
@@ -11,3 +11,25 @@ const user = {
 user.welcomeMessage()
 user.username = "sid"
 user.welcomeMessage()     
+
+const arrow= () => {
+    let username = "sid"
+    console.log(this)  // does not work
+
+}
+
+
+// arrow function
+// if in curly braces return is must
+//explicet return 
+const addTow = (num1, num2) => {
+    return num1+num2
+}
+
+//user in react
+// retrun not required
+//implicit return 
+const Two = (num1, num2) => num1 + num2
+
+// console.log(Two(3,4))
+
